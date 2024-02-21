b6d2b5dec78f0723e76087ce642d601dca1d9966
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-02-21 19:06:22 +0530

    Deploy

---
+++ src/main/java/com/excelr/WecomeRestApi1Application.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecomeRestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WecomeRestApi1Application.class, args);
+	}
+
+}
